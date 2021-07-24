4c9772b702be5896df0417b3532493340883d455
Sunwoo Kim <s.kim32415@gmail.com>
2021-07-24 11:33:17 -0700

    extracting state abbreviations from data

--- static-sunwoo/js/app.js
+++ static-sunwoo/js/app.js
@@ -1,8 +1,16 @@
 // Use D3 to read data from json file
-d3.csv("data/us-widlfires.csv").then(function(data) {
+d3.csv("data/us-wildfires.csv").then(function(data) {
 
   console.log(data);
+  var states = [];
+  data.forEach(fire=> {
+    if(!states.includes(fire.STATE)) {
+      states.push(fire.STATE);
+    }
+  });
+  console.log(`Here are the states ${states}`);
+  console.log(states.length);
 
 });
 
-console.log("hello")
+console.log("hello");
