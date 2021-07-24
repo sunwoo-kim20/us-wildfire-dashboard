a7729857960239239af3ea080e7ee771bcfd510b
Sunwoo Kim <s.kim32415@gmail.com>
2021-07-24 10:38:04 -0700

    updated test html and javascript

--- static-sunwoo/js/app.js
+++ static-sunwoo/js/app.js
@@ -1,6 +1,6 @@
 // Use D3 to read data from json file
 d3.csv("data/us-widlfires.csv").then(function(data) {
-  // console.log(data.samples.filter(d => d.id === "940"));
+
   console.log(data);
 
 });
