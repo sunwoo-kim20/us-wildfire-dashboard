d18e0c1723c7f8b26ad486d418a0205947cecb18
Sunwoo Kim <s.kim32415@gmail.com>
2021-07-24 12:01:41 -0700

    added event listeners

--- static-sunwoo/js/app.js
+++ static-sunwoo/js/app.js
@@ -8,8 +8,24 @@ d3.csv("data/us-wildfires.csv").then(function(data) {
       states.push(fire.STATE);
     }
   });
+  states = states.sort();
   console.log(`Here are the states ${states}`);
   console.log(states.length);
+
+  // Add ids to dropdown menu
+  var dropDown = d3.select("#selDataset");
+  states.forEach(function(abbrev) {
+    dropDown.append("option")
+      .attr("value", abbrev)
+      .text(abbrev);
+  });
+
+  // Set up event for dropdown and form
+  var dropdown = d3.select("#selDataset");
+  var form = d3.select("#form");
+
+  dropdown.on("change", genCharts);
+  form.on("submit", genCharts);
 
 });
 
